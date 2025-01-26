d94ab991ac1e91eb366e71ff4ad2e2d3e696e9fa
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2025-01-26 19:22:00 +0300

    complete lab

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks, onStockClick }) {
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.length > 0 ? (
+        stocks.map((stock) => (
+          <Stock key={stock.id} stock={stock} onClick={onStockClick} />
+        ))
+      ) : (
+        <p>No stocks available.</p>
+      )}
     </div>
   );
 }
